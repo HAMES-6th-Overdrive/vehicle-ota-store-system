cc6d9cce644d56c33179a7019007ef3915033af4
jaedong1 <106041575+jaedong1@users.noreply.github.com>
2026-05-30 21:50:48 +0900

    test

--- src/firmware-front-zcu/Cpu0_Main.c
+++ src/firmware-front-zcu/Cpu0_Main.c
@@ -106,7 +106,6 @@ void core0_main(void)
     IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
     
     /* SOTA 초기화: SWAPEN disabled이면 next entry에 Bank A marker를 기록하고 SWAPEN enable 후 reset */
-    AppDebug_Init();
     SOTA_InitialSetup();
     AppDebug_Init();
     
